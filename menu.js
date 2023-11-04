8cd5e9a02b1c29e169228c626242ef7535f46c86
Joel-Caliba01~ <joel819688@gmail.com>
2023-11-04 01:27:41 -0300

    agregado de login

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+// NAV DE MOVIL
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
@@ -9,3 +10,40 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+// LOGIN 
+
+// PARTE DE LOGIN Y REGISTER PARA LA ANIMACION
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+// AL PRECIONAR CUENTA SE ABRE EL FORMULARIUO LOGIN
+
+// Obtén una referencia al botón "Cuenta" por su ID
+var botonCuenta = document.getElementById("botonCuenta");
+
+// Obtén una referencia a la sección "cuenta" por su clase
+var seccionCuenta = document.querySelector(".cuenta");
+
+// Agrega un evento de clic al botón
+botonCuenta.addEventListener("click", function() {
+    // Verifica si la sección "cuenta" está visible
+    if (seccionCuenta.style.display === "none" || seccionCuenta.style.display === "") {
+        // Si está oculta, muéstrala
+        seccionCuenta.style.display = "block";
+    } else {
+        // Si está visible, ocúltala
+        seccionCuenta.style.display = "none";
+    }
+});
